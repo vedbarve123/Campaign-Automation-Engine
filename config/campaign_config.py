a6dd1bf86af4fe6507459cfdefcb1a46e5dbeb4c
Ved Barve <vedbarve123@gmail.com>
2026-07-30 20:45:00 +0530

    Feature: SQL Builder 1

--- config/campaign_config.py
+++ config/campaign_config.py
@@ -8,17 +8,17 @@ class Question:
     segment_name: str
     customer_types:List[str]=field(default_factory=list)
     recency:List[str] = field(default_factory=list)
-    min_total_orders:Optional[int]=None
-    max_total_orders:Optional[int]=None
+    # min_total_orders:Optional[int]=None
+    # max_total_orders:Optional[int]=None
     min_frequency:Optional[int]=None
-    max_frequency:Optional[int]=None
-    min_orders_last_1yr:Optional[int]=None
-    max_orders_last_1yr:Optional[int]=None
+    # max_frequency:Optional[int]=None
+    # min_orders_last_1yr:Optional[int]=None
+    # max_orders_last_1yr:Optional[int]=None
     min_quantity:Optional[int]=None
-    max_quantity:Optional[int]=None
+    # max_quantity:Optional[int]=None
     min_revenue:Optional[float]=None
-    max_revenue:Optional[float]=None
-    min_avg_order_value:Optional[float]=None
+    # max_revenue:Optional[float]=None
+    # min_avg_order_value:Optional[float]=None
     fav_stores:List[str] = field(default_factory=list)
     stores: List[str] = field(default_factory=list)
     products:List[str] = field(default_factory=list)
---
+++ services/sql_builder.py
@@ -0,0 +1,80 @@
+from config.campaign_config import Question
+
+class SQLBuilder:
+    def build(self,question:Question)->str:
+        select_clause=self._build_select()
+        from_clause=self._build_from()
+        where_clause=self._build_where(question)
+        query = f"""
+        {select_clause}
+        {from_clause}
+        {where_clause}
+    """
+        return query.strip()
+    
+    def _build_select(self)->str:
+        return """
+        SELECT mobile
+    """
+    
+    def _build_from(self)->str:
+        return """
+        FROM RetailCo.customer_single_view
+    """
+    
+    def _build_where(self,question:Question)->str:
+        conditions=[]
+        
+        if question.customer_types:
+            customer_types = [
+                f"'{customer_type}'"
+                for customer_type in question.customer_types
+                            ]
+            sql_customer_types=",".join(customer_types)
+            
+            conditions.append(
+                f"customer_type IN ({sql_customer_types})"
+            )
+
+        if question.recency:
+            recency_value = [
+                f"'{value}'"
+                for value in question.recency
+                            ]
+            sql_recency=",".join(recency_value)
+            
+            conditions.append(
+                f"recency IN ({sql_recency})"
+            )
+
+        if question.fav_stores:
+            fav_stores = [
+                f"'{fav_store}'"
+                for fav_store in question.fav_stores
+                            ]
+            sql_fav_stores=",".join(fav_stores)
+            
+            conditions.append(
+                f"fav_stores IN ({sql_fav_stores})"
+            )
+
+        if question.min_frequency is not None:
+            conditions.append(
+                f"frequency >= {question.min_frequency}"
+            )
+        
+        if question.min_quantity is not None:
+            conditions.append(
+                f"quantity >= {question.min_quantity}"
+            )
+
+        if question.min_revenue is not None:
+            conditions.append(
+                f"revenue >= {question.min_revenue}"
+            )
+
+
+        if  conditions:
+            return "WHERE " + " AND ".join(conditions)
+        else:
+            return ""
